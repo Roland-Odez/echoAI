b7efe7979c90b02dfe7197af468bb9a805033466
Roland Odenore - jsboy <breezyroland@gmail.com>
2024-06-09 15:51:42 +0100

    added custom data on homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
 import Leftsidebar from "@/components/Leftsidebar";
+import MobileNav from "@/components/MobileNav";
+import Rightsidebar from "@/components/Rightsidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "echoAI - Home page",
@@ -12,11 +15,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
             <Leftsidebar />
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
+
+            <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+              <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                <div className="flex h-16 items-center justify-between md:hidden">
+                  <Image
+                    src='/icons/logo.svg'
+                    width={30}
+                    height={30}
+                    alt="menu icon"
+                  />
+                  <MobileNav />
+                </div>
+                <div className="flex flex-col md:pd-14">
+                  Toaster (notificatipn)
+                  {children}
+                </div>
+              </div>
+            </section>
+            <Rightsidebar />
         </main>
     </div>
   );
---
+++ components/Rightsidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rightsidebar = () => {
+  return (
+    <section className='right_sidebar text-white'>
+        right side bar
+    </section>
+  )
+}
+
+export default Rightsidebar
