a9a8587d0aeed82c847fbbda2c8f6daab34455fc
Roland Odenore - jsboy <breezyroland@gmail.com>
2024-06-19 03:23:25 +0100

    complemented create podcast

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,17 +1,22 @@
+import { PodcastCardProps } from '@/types'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import React from 'react'
 
-const PodcastCard = ({imgURL, title, description, podcastId}: {
-  imgURL: string,
-  title: string,
-  description: string,
-  podcastId: number
-}) => {
+const PodcastCard = ({imgUrl, title, description, podcastId}: PodcastCardProps) => {
+
+  const router = useRouter()
+
+  const handleViews = () => {
+    //increae views
+    router.push(`/podcast/${podcastId}`)
+  }
+
   return (
-    <div className='cursor-pointer'>
+    <div className='cursor-pointer' onClick={handleViews}>
       <figure className='flex flex-col gap-2'>
         <Image
-          src={imgURL}
+          src={imgUrl}
           width={174}
           height={174}
           alt={title}
